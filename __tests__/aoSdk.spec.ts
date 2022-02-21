4527aca081bbec660b41ffed2d84b193ec68bbc1
luso <lukas.solberg@knightec.se>
2022-02-21 15:01:36 +0100

    update git_push script

--- __tests__/aoSdk.spec.ts
+++ __tests__/aoSdk.spec.ts
@@ -1,12 +1,12 @@
 import SmartAccess from "../";
 
-const SC = new SmartAccess();  
+const SA = new SmartAccess();  
 
 describe("SC SDK for TypeScript", () => {
   it("can be imported and initialized", async () => {
     try {
       const requestingServiceProvider =
-        await SC.serviceProviders.getServiceProvider("sp-id");
+        await SA.serviceProviders.getServiceProvider("sp-id");
     } catch (error) {
       expect(error).toBeDefined();
     }
--- git_push.sh
+++ git_push.sh
@@ -1,35 +1,29 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./git_push.sh "minor update"
+# Usage example: ./git_push.sh "minor update"
 
 release_note=$1
-
 git_repo_id=Smart-Consent-Access/sa-sdk-ts
+
+# Creates a temporary folder and clone the SDK repo into it
+mkdir temp
+(cd temp && git clone git@github.com:$git_repo_id.git)
+
+# Copy the fils over to the temporary folder
+rsync -rv --exclude=temp . temp/sa-sdk-ts/
 
 if [ "$release_note" = "" ]; then
     release_note="Minor update"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
-# Initialize the local directory as a Git repository
-git init
-
-# Adds the files in the local repository and stages them for commit.
-git add .
-
-# Commits the tracked changes and prepares them to be pushed to a remote repository. 
-git commit -m "$release_note"
-
-# Sets the new remote
-git_remote=`git remote`
-if [ "$git_remote" = "" ]; then # git remote not defined
-    git remote add origin git@github.com:$git_repo_id.git
-fi
-
-git pull origin master
+# Adds the files and commit them.
+cd temp/sa-sdk-ts && git add . && git commit -m "$release_note"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to git@github.com:$git_repo_id.git"
 git push origin master 2>&1 | grep -v 'To https'
 
+# Remove the temporary folder
+cd ../../ && rm -rf temp/
